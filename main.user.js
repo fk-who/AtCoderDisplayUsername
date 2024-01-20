fe6852a67dd26522df3c3921e955019945bda4c4
fjkmg <45952260+fk-who@users.noreply.github.com>
2024-01-20 16:41:33 +0900

    add updateURL, downloadURL, supportURL, namespace

--- main.user.js
+++ main.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         AtCoder display username
+// @namespace    https://github.com/fk-who
 // @version      0.01
 // @description  AtCoderで特定ユーザの提出を検索した際、タイトルにユーザー名を表示するuserscript
 // @author       fjkmg
 // @match        https://atcoder.jp/contests/*/submissions?*f.User=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atcoder.jp
 // @grant        none
+// @updateURL    https://github.com/fk-who/AtCoderDisplayUsername/raw/main/main.user.js
+// @downloadURL  https://github.com/fk-who/AtCoderDisplayUsername/raw/main/main.user.js
+// @supportURL   https://github.com/fk-who/AtCoderDisplayUsername/
 // ==/UserScript==
 
 
